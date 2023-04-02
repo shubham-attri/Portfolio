d01c774c0302fdd4ced1189686f9ec22cda88d3f
Shubham Attri <shubham21354@iiitd.ac.in>
2023-04-02 20:47:47 +0530

    update 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -999,3 +999,22 @@ new Cube({
   viewport: viewport,
   element: document.getElementsByClassName('cube')[0]
 });
+
+
+//Gallery
+const track = document.getElementById("image-track");
+window.onmousedown = e => {
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onMouseMove = e =>{
+  if(track.dataset.mouseDownAt ==="0") return;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) -e.clientX,
+      maxDelta = window.innerWidth/2;
+  const  percentage = (mouseDelta/maxDelta)*100;
+  track.style.transform ='translate(${percentage}%,50%)';
+}
+
+window.onmouseup = ()=>{
+  track.dataset.mouseDownAt ="0";
+}
